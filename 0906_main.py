f3febd747a5d2927caf62425122151016d7b3a79
naofujii <fujiinao0305@gmail.com>
2024-09-08 17:44:19 +0900

    first commit

--- 0906_main.py
+++ 0906_main.py
@@ -20,23 +20,23 @@ with st.form("version_form"):
    
     st.form_submit_button('更新')
 
-df_review_random_normal = pd.read_csv(f'実行結果/0906/test_data_streamlit/{target_product}.csv', index_col=0)
+df_review_random_normal = pd.read_csv(f'test_data_streamlit/{target_product}.csv', index_col=0)
 
-random_list = list(pd.read_csv('実行結果/0906/test_data_streamlit/random_list.csv',index_col=0)['0'])
+random_list = list(pd.read_csv('test_data_streamlit/random_list.csv',index_col=0)['0'])
     
 
 st.write('現在のバージョン：', ver)
 col1, col2 = st.columns([2, 1], gap='large')
 
 try:
-    df_review_random = pd.read_csv(f'実行結果/0906/test_data_streamlit/{target_product}_{ver}.csv', index_col=0)
+    df_review_random = pd.read_csv(f'test_data_streamlit/{target_product}_{ver}.csv', index_col=0)
 except FileNotFoundError:
     col1.warning('バージョンを入力してください')
 
 
 with col2:
     st.subheader("選択項目")
-    df_topic = pd.read_csv('実行結果/0906/トピックリスト.csv',index_col=0)
+    df_topic = pd.read_csv('test_data_streamlit/トピックリスト.csv',index_col=0)
     
     with st.form("topic_form", clear_on_submit=False): 
         submitted = st.form_submit_button("Submit")
@@ -56,7 +56,7 @@ with col2:
     if submitted:
         if count != None:
             df_review_random.at[count, 'トピック'] = str(options)
-            df_review_random.to_csv(f'実行結果/0906/test_data_streamlit/{target_product}_{ver}.csv')
+            df_review_random.to_csv(f'test_data_streamlit/{target_product}_{ver}.csv')
         
     
     
