1e77e3798fe0793d3d0a261738ac25cd01dbf440
naofujii <fujiinao0305@gmail.com>
2024-09-08 19:18:13 +0900

    update style

--- 0906_main.py
+++ 0906_main.py
@@ -10,6 +10,7 @@ st.title('口コミのトピック分類')
 
 def create_csv():
     df_review_random = df_review_random_normal
+    count = -1
     
 
 with st.form("version_form"):
@@ -26,7 +27,9 @@ df_review_random_normal = pd.read_csv(f'./test_data_streamlit/{target_product}.c
 
 random_list = list(pd.read_csv(f'./test_data_streamlit/random_list.csv',index_col=0)['0'])
     
-
+if ver = "":
+    st.warning('バージョンを入力してください')
+    
 st.write('現在のバージョン：', ver)
 col1, col2 = st.columns([2, 1], gap='large')
 
@@ -59,10 +62,6 @@ with col2:
         if count != None:
             df_review_random.at[count, 'トピック'] = str(options)
             df_review_random.to_csv(f'./test_data_streamlit/{target_product}_{ver}.csv')
-        
-    
-    
-with col1:
     
     @st.cache_data
     def convert_df(df):
@@ -72,12 +71,15 @@ with col1:
     csv = convert_df(df_review_random)
 
     st.download_button(
-        label="Download",
+        label="入力したデータを保存する",
         data=csv,
         file_name=f'{target_product}_{ver}.csv',
         mime="text/csv",
     )
     
+    
+    
+with col1:
     st.subheader("口コミ本文")
     
     if count != None:
